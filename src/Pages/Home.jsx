a80bc940b142db060a7f109e03c83fec0c2b5ba1
zaxwtf <zaxwtf06@gmail.com>
2026-06-18 16:53:07 +0200

    refactor: agregado contenido para home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,21 @@
+import Card from "../components/Card"
+
+
+
 function Home(){
     return(
-        <h1>Home Page</h1>
+        <>
+        <h1>GamerHub</h1>
+        <h2>¡Todos tus Videojuegos favoritos al alcance de un click!</h2>
+
+        <section className="funciones">
+            <Card title= "Biblioteca extensa" description= "Encuentra cualquier videojuego que busques"/>
+            <Card title= "Sistema de Favs" description= "podrás asegurarte de tener a mano cualquier juego que necesites"/>
+            <Card title= "En colección" description= "Lleva un registro de todos los juegos que tienes en tu colección"/>
+        </section>
+        
+        </>
+        
     )
 }
 
