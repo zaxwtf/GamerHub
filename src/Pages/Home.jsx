11857d75dd4aca52a32ade2a00d83386f47394c5
zaxwtf <zaxwtf06@gmail.com>
2026-06-23 23:45:25 +0200

    feat: modificado estilos a tailwind y agregado loadVideogames

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom"
 import Card from "../components/Card"
+import { LoadVideogames } from "../components/LoadVideogames"
 
 import { RegisterForm } from "../components/RegisterForm"
 
 function Home(){
     return(
-        <div className="home">
-            <div className="bannerHome">
+        <div className="flex flex-col items-center ">
+            <div className="bannerHome ">
                 <img src="/src/assets/fondo_GamerHub.jpg" alt="Fondo GamerHub" />
                 <h1>GamerHub</h1>
             </div>
-            <RegisterForm/>
-            <h2>¡Todos tus Videojuegos favoritos al alcance de un click!</h2>
-
-            <section className="funciones">
+            <h2 className="text-3xl my-8 font-black ">¡Todos tus Videojuegos favoritos al alcance de un click!</h2>
+            <LoadVideogames/>
+            <section className="flex justify-center gap-[5vw] mx-0 mt-[10px]">
                 <Card title= "Biblioteca extensa" description= "Encuentra cualquier videojuego que busques"/>
                 <Card title= "Sistema de Favs" description= "podrás asegurarte de tener a mano cualquier juego que necesites"/>
                 <Card title= "En colección" description= "Lleva un registro de todos los juegos que tienes en tu colección"/>
             </section>
 
-            <Link to= "/videogames">¡Unete a nosotros!</Link>
+            <h2 className="text-3xl mt-8 font-black">¡Unete a nosotros!</h2>
+            <RegisterForm/>
         
         </div>
         
