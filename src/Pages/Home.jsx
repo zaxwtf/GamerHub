3f66d811c2e5e445dfcd12196430e9c01d3a151c
zaxwtf <zaxwtf06@gmail.com>
2026-06-23 15:11:15 +0200

    feat: nuevass secciones y aspecto visual para pagina home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,27 @@
+import { Link } from "react-router-dom"
 import Card from "../components/Card"
 
-
+import { RegisterForm } from "../components/RegisterForm"
 
 function Home(){
     return(
-        <>
-        <h1>GamerHub</h1>
-        <h2>¡Todos tus Videojuegos favoritos al alcance de un click!</h2>
+        <div className="home">
+            <div className="bannerHome">
+                <img src="/src/assets/fondo_GamerHub.jpg" alt="Fondo GamerHub" />
+                <h1>GamerHub</h1>
+            </div>
+            <RegisterForm/>
+            <h2>¡Todos tus Videojuegos favoritos al alcance de un click!</h2>
 
-        <section className="funciones">
-            <Card title= "Biblioteca extensa" description= "Encuentra cualquier videojuego que busques"/>
-            <Card title= "Sistema de Favs" description= "podrás asegurarte de tener a mano cualquier juego que necesites"/>
-            <Card title= "En colección" description= "Lleva un registro de todos los juegos que tienes en tu colección"/>
-        </section>
+            <section className="funciones">
+                <Card title= "Biblioteca extensa" description= "Encuentra cualquier videojuego que busques"/>
+                <Card title= "Sistema de Favs" description= "podrás asegurarte de tener a mano cualquier juego que necesites"/>
+                <Card title= "En colección" description= "Lleva un registro de todos los juegos que tienes en tu colección"/>
+            </section>
+
+            <Link to= "/videogames">¡Unete a nosotros!</Link>
         
-        </>
+        </div>
         
     )
 }
