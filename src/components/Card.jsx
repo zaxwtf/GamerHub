e8a102a4807e930a8882894922cb8dbfa6bcb93e
zaxwtf <zaxwtf06@gmail.com>
2026-06-24 00:23:44 +0200

    refactor: cambiada medida de las cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card({title, description}){
     return(
-        <div className="rounded border border-black p-2 min-h-40 w-100">
+        <div className="rounded border border-black p-2 min-h-40 w-90">
             <h2>{title}</h2>
             <p>{description}</p>
         </div>
