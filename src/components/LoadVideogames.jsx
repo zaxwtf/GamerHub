cc75387cbde69079aeaee2512eb432439a06861e
zaxwtf <zaxwtf06@gmail.com>
2026-06-23 23:44:27 +0200

    feat: agregado estilos tailwind para mostrar juegos con efecto infinite scroll

---
+++ src/components/LoadVideogames.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+
+function LoadVideogames(){
+        const [videogames, setVideogames] = useState(null)
+        const [isLoading, setIsLoading] = useState(false)
+        const [error, setError] = useState(null)
+        const [page, setPage] = useState(1)
+
+        const API = import.meta.env.API_URL
+    
+        useEffect(()=>{
+            async function load() {
+                try {
+                    setIsLoading(true)
+                    setError(null)
+                    const response = await fetch(`http://localhost:3000/api/juegos/`)
+                    if (!response.ok) throw new Error("No se pudo cargar el videojuego")
+                    const data = await response.json()
+                    setVideogames(data)
+                } catch (error) {
+                    setError(error.message)
+                }finally{
+                    setIsLoading(false)
+                }
+            }
+            load()
+        }, [page])
+    
+    
+    
+        return(
+            <div className="flex h-75 w-[90vw] overflow-x-auto no-scrollbar"> 
+                {isLoading && <p>Cargando...</p>}
+                {error && <p>Error:</p>}
+                {!error && !isLoading && (
+                    <div className="flex  gap-5 animate-infinite-scroll">
+                        {videogames?.map((videogame)=>{
+                            if (videogames.indexOf(videogame) < 20){
+                                console.log(videogame)
+                            return <Link className="" to={`/videogames/${videogame._id}`} key={videogame._id}>
+                                <img className="h-70 min-w-60 " src={videogame.img} alt={videogame.nombre} />
+                                </Link>}
+                        })}
+                    </div>
+                        
+                )}
+            </div>
+        )
+    
+    }
+
+
+export{
+    LoadVideogames
+}
