01794a02e8128cc85c3817595c0911eb8a28e8c1
zaxwtf <zaxwtf06@gmail.com>
2026-06-23 15:10:16 +0200

    refactor: borrado link de los imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Routes, Route, Link} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import { Home } from './Pages/Home'
 import { About } from './Pages/About'
 import { Videogames } from './Pages/Videogames'
