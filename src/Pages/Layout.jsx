97eabad2501b6bb23fa2b7ca1f80e75118b46b56
zaxwtf <zaxwtf06@gmail.com>
2026-06-23 16:39:06 +0200

    refactor: borrado themeswitcher del layout

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,6 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom"
-import { ThemeSwitcher } from "../components/ThemeSwitcher"
-import { Header } from "../components/Header"
+
 
 
 function Layout(){
@@ -10,8 +9,8 @@ function Layout(){
                 | <NavLink to= "/">Inicio</NavLink> |
                 <NavLink to= "/videogames"> Videojuegos</NavLink> |
                 <NavLink to= "/about"> Acerca de</NavLink> |
+                <NavLink to="/register">Registro</NavLink> |
             </nav>
-            <ThemeSwitcher/>
             <main>
                 <Outlet/>
             </main>
