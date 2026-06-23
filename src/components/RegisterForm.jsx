621fce874bf71d0ba6d02a1dd6226dace35e4505
zaxwtf <zaxwtf06@gmail.com>
2026-06-23 23:44:51 +0200

    refactor: modificado estilos

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -31,15 +31,15 @@ function RegisterForm(){
         <form onSubmit={handleSubmit} className="mx-auto flex max-w-md flex-col gap-4 p-6 bg-white">
             <div className="flex flex-col gap-1">
                 <label className="text-xl">Name</label>
-                <input onChange={handleChange} type="text" name="name" value={data.name} className="rounded border border-gray-300 px-3 focus:border-purple-500 focus:outline-purple-500"/>
+                <input onChange={handleChange} type="text" name="name" value={data.name} className="rounded border border-gray-300 px-3 hover:border-purple-500 focus:outline-purple-500"/>
                 {errors.name && <span>{errors.name}</span>}
                 <label className="text-xl">Email</label>
-                <input onChange={handleChange} type="email" name="email" value={data.email} className="rounded border border-gray-300 px-3 focus:border-purple-500 focus:outline-purple-500" />
+                <input onChange={handleChange} type="email" name="email" value={data.email} className="rounded border border-gray-300 px-3 hover:border-purple-500 focus:outline-purple-500" />
                 {errors.email && <span>{errors.email}</span>}
                 <label className="text-xl">Password</label>
-                <input onChange={handleChange} type="password" name="password" value={data.password} className="rounded border border-gray-300 px-3 focus:border-purple-500 focus:outline-purple-500" />
+                <input onChange={handleChange} type="password" name="password" value={data.password} className="rounded border border-gray-300 px-3 hover:border-purple-500 focus:outline-purple-500" />
                 {errors.password && <span>{errors.password}</span>}
-                <button className="rounded border border-purple-500 px-30 mt-6 hover:bg-purple-600 transition duration-400">Enviar</button>
+                <button className="rounded border border-purple-500 px-3 mt-6 hover:bg-purple-600 transition duration-400">Enviar</button>
             </div>
         </form>
     )
