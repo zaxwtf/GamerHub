71d98a4951e35373964d2f536dc9916ae47a1d80
zaxwtf <zaxwtf06@gmail.com>
2026-06-24 16:49:14 +0200

    refactor: cambiado aspecto del main nav, agrandado tamaño, centrado y establecido fondo de color morado con baja opacidad, colocado en position fixed

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -4,12 +4,12 @@ import { NavLink, Outlet } from "react-router-dom"
 
 function Layout(){
     return (
-        <div className="layout">
-            <nav className="mainNav">
-                | <NavLink to= "/">Inicio</NavLink> |
-                <NavLink to= "/videogames"> Videojuegos</NavLink> |
-                <NavLink to= "/about"> Acerca de</NavLink> |
-                <NavLink to="/register">Registro</NavLink> |
+        <div className="felx felx-col">
+            <nav className="flex gap-15 justify-center p-5 text-center text-2xl text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
+                <NavLink to= "/">Inicio</NavLink>
+                <NavLink to= "/videogames">Videojuegos</NavLink>
+                <NavLink to= "/about">Acerca de</NavLink>
+                <NavLink to="/register">Registro</NavLink>
             </nav>
             <main>
                 <Outlet/>
