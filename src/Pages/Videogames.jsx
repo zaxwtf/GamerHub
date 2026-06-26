9ddb13a5d327ca83e38af1bcb57799df1bca7612
zaxwtf <zaxwtf06@gmail.com>
2026-06-26 19:09:44 +0200

    refactor: borrado botones de pasar pagina no utilizados

--- src/Pages/Videogames.jsx
+++ src/Pages/Videogames.jsx
@@ -28,23 +28,18 @@ function Videogames(){
     
     
         return(
-            <>
-            <h1>Videojuegos</h1>
-                <div>
-                    <button onClick={()=>{setPage(page - 1)}} disabled= {page === 1}>Anterior</button>
-                    <span>Página {page}</span>
-                    <button onClick={()=>{setPage(page + 1)}}>Siguiente</button>
-                </div>
-                {isLoading && <p>Cargando...</p>}
-                {error && <p>Error:</p>}
-                {!error && !isLoading && (
-                    <ul>
-                        {videogames?.map((videogame)=>{
-                            return <li><Link to= {`/videogames/${videogame._id}`} key={videogame._id}>{videogame.nombre}</Link></li>
-                        })}
-                    </ul>
-                )}
-            </>
+            <div className="pt-20">
+                <h1>Videojuegos</h1>
+                    {isLoading && <p>Cargando...</p>}
+                    {error && <p>Error:</p>}
+                    {!error && !isLoading && (
+                        <ul>
+                            {videogames?.map((videogame)=>{
+                                return <li><Link to= {`/videogames/${videogame._id}`} key={videogame._id}>{videogame.nombre}</Link></li>
+                            })}
+                        </ul>
+                    )}
+            </div>
         )
     
     }
