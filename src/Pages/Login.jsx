1e5524aa83bd1bb461d64d599587ebcd43eea0c6
zaxwtf <zaxwtf06@gmail.com>
2026-06-26 16:19:17 +0200

    feat: creada pagina de login

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "../components/LoginForm"
+
+function Login(){
+    return(
+        <LoginForm className= "bg-red-500 mt-20"></LoginForm>
+    )
+}
+
+export{
+    Login
+}
