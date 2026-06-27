e1666c6190dd774bc9f25ed99ca1de7a6f3ee083
zaxwtf <zaxwtf06@gmail.com>
2026-06-27 19:47:42 +0200

    refactor: quitado estilosde layout

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -6,8 +6,8 @@ function Layout(){
     const {token} = useAuth()
     if(!token){
         return (
-        <div className="felx felx-col">
-            <nav className="flex gap-15 justify-center p-5 text-center text-2xl text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
+        <div>
+            <nav className="flex flex-wrap gap-7 justify-center p-5 text-center text-xs sm:text-lg sm:gap-10 md:gap-15 text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
                 <NavLink to= "/">Inicio</NavLink>
                 <NavLink to= "/videogames">Videojuegos</NavLink>
                 <NavLink to= "/about">Acerca de</NavLink>
@@ -21,8 +21,8 @@ function Layout(){
     )
     }
     return (
-        <div className="felx felx-col">
-            <nav className="flex gap-15 justify-center p-5 text-center text-2xl text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
+        <div>
+            <nav className="flex flex-wrap gap-7 justify-center p-5 text-center text-xs sm:text-lg sm:gap-10 md:gap-15 text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
                 <NavLink to= "/">Inicio</NavLink>
                 <NavLink to= "/videogames">Videojuegos</NavLink>
                 <NavLink to= "/about">Acerca de</NavLink>
