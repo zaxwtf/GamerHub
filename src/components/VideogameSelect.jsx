d937bd0436e23bedcd929d41dc5610c0518d6922
zaxwtf <zaxwtf06@gmail.com>
2026-06-27 13:11:05 +0200

    feat: componente de selector de consolas para los juegos

---
+++ src/components/VideogameSelect.jsx
@@ -0,0 +1,23 @@
+// function VideogameSelect(){
+
+
+
+
+// return(
+//     <div>
+//         <select className="text-red-500" name="plataformas" id="plataformas">
+//             <option value="todos">todos</option>
+//             <option value="PS4">PS4</option>
+//             <option value="PS5">PS5</option>
+//             <option value="PS2">PS2</option>
+//             <option value="XBOX360">Xbox 360</option>
+//             <option value="Nintendo Switch">Nintendo Switch</option>
+//             <option value="XBOX One">Xbox One</option>
+//             <option value="Nintendo 3ds">Nintendo 3ds</option>
+//         </select>
+//     </div>
+// )
+// }
+
+
+
