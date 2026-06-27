61c634256b97e3dbc3c3e9e4ad072ca48d8efbef
zaxwtf <zaxwtf06@gmail.com>
2026-06-27 10:55:02 +0200

    feat: agregada verificación de si existe token para que se muestre un menu en el layout u otro , el sin token muestra las opciones de registro y login y si hay token muestra el perfil

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,9 +1,11 @@
 import { NavLink, Outlet } from "react-router-dom"
-
+import { useAuth } from "../hooks/useAuth"
 
 
 function Layout(){
-    return (
+    const {token} = useAuth()
+    if(!token){
+        return (
         <div className="felx felx-col">
             <nav className="flex gap-15 justify-center p-5 text-center text-2xl text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
                 <NavLink to= "/">Inicio</NavLink>
@@ -11,6 +13,20 @@ function Layout(){
                 <NavLink to= "/about">Acerca de</NavLink>
                 <NavLink to="/register">Registro</NavLink>
                 <NavLink to="/login">Login</NavLink>
+            </nav>
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+    )
+    }
+    return (
+        <div className="felx felx-col">
+            <nav className="flex gap-15 justify-center p-5 text-center text-2xl text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
+                <NavLink to= "/">Inicio</NavLink>
+                <NavLink to= "/videogames">Videojuegos</NavLink>
+                <NavLink to= "/about">Acerca de</NavLink>
+                <NavLink to="/profile">Perfíl</NavLink>
             </nav>
             <main>
                 <Outlet/>
