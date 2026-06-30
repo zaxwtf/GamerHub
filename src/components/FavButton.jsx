18171567035a5bb7e8455ce06064a6fec48773ab
zaxwtf <zaxwtf06@gmail.com>
2026-06-30 18:53:35 +0200

    fix: funcionamiento arreglado de favButton, creado useState isFav para comprobar si esta en fav, creda funcion toogleFav para que al clicar sobre el boton se agregue o se elimine el juego de la lsita de favoritos haciendo un post o un delete cuando toque

--- src/components/FavButton.jsx
+++ src/components/FavButton.jsx
@@ -1,37 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
-import { useFetch } from "../hooks/useFetch";
 
 
-function FavButton({juegoID}) {
-    const {loadProfile, token} = useAuth()
-    const [usuario, setUsuario] = useState(null)
+function FavButton({gameId, favValue}) {
+    const {token} = useAuth()
+    const [isFav, setIsFav] = useState(favValue)
     
-    useEffect(()=>{
-        async function getProfile() {
-            const userData = await loadProfile(token)
-            setUsuario(userData)
+    
+
+        async function toogleFav(){
+            const url = isFav ? "https://backendproyect-m2.onrender.com/api/usuarios/profile/favs/delete" : "https://backendproyect-m2.onrender.com/api/usuarios/profile/favs"
+            const method = isFav ? "DELETE" : "POST"
+            const response = await fetch(url, {
+            method: method,
+            headers: {"Content-Type": "application/json", Authorization: `Bearer ${token}`},
+            body: JSON.stringify({gameId})
+        })
+        if (!response.ok) throw new Error("Credenciales incorrectas")
+            setIsFav(!isFav)
         }
-        getProfile()
-    }, [])
-
-    function inFav(){
-        return usuario?.user.juegosFav.includes(juegoID)
-    }
-
-    function deleteGameFav(){
-        const {data, isLoading, error} = useFetch("https://backendproyect-m2.onrender.com/api/usuarios/profile/favs/delete", {method: "DELETE", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({gameId: juegoID})})
-    }
-
-    async function handleFavGames() {
-        inFav() ? useFetch("https://backendproyect-m2.onrender.com/api/usuarios/profile/favs/delete", {method: "DELETE", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({gameId: juegoID})}) : "Guardar en favoritos"
-    }
 
     
 
     return(
-        <button className="bg-purple-500 border-2 rounded-lg p-1 hover:bg-purple-700">
-            {inFav() ? "Quitar de favoritos" : "Guardar en favoritos"}
+        <button onClick={toogleFav} className="bg-purple-500 border-2 rounded-lg p-1 hover:bg-purple-700">
+            {isFav ? "Quitar juego de favoritos" : "Agregar juego a favoritos"}
         </button>
     )
 }
