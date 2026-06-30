8776ef931cf5ab1d64de520aef88831a6244d51f
zaxwtf <zaxwtf06@gmail.com>
2026-06-30 16:36:30 +0200

    styles: cambiado estilos de about page para que el titulo este mejor colocado

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,7 @@
 function About(){
     return(
-        <div className="pt-15 pl-5 flex flex-col gap-7">
-            <h1 className="text-2xl font-black">Sobre nosotros</h1>
+        <div className="pt-17 pl-5 flex flex-col gap-7">
+            <h1 className="text-3xl font-black">Sobre nosotros</h1>
             <p >Somos una organización sin animo de lucro que quiere facilitarle la vida a los gamers y coleccionistas con una aplicación donde puedan encontrar cualquier juego que busquen y que puedan guardarlos para así llevar una organización de los juegos que tienen</p>
             <p>Para contactar con nostros puede hacerlo desde nuestro email: <b>contacto@gamerhub.com</b></p>
         </div>
