33c2c78266eb6009bdd4295cf7ae94b04c443f76
zaxwtf <zaxwtf06@gmail.com>
2026-06-30 19:35:16 +0200

    feat: creado boton para cerrar sesion

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../hooks/useAuth"
 import { GetFavGames } from "../components/GetFavGames"
 
 export function Profile(){
-    const {loadProfile, token} = useAuth()
+    const {loadProfile, token, logout} = useAuth()
     const [usuario, setUsuario] = useState(null)
     
     useEffect(()=>{
@@ -22,6 +22,7 @@ return(
         <div className="ml-5">
             <h1 className="pt-20 text-3xl font-black">¡Hola, {usuario?.user.userName}!</h1>
             <h2 className="text-xl mt-6">Lista de juegos favoritos</h2>
+            <button className="border-2 rounded-lg p-1 bg-purple-500 hover:bg-purple-700 mt-2" onClick={logout}>Cerrar sesión</button>
             <div>
                 <GetFavGames/>
             </div>
