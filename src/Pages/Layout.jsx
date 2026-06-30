97b13971208d886b511d7a120610b77cdb9284ff
zaxwtf <zaxwtf06@gmail.com>
2026-06-30 16:37:04 +0200

    styles: agregado efecto blur al layout para que todo lo que pase por debajo se vea borroso

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -7,7 +7,7 @@ function Layout(){
     if(!token){
         return (
         <div>
-            <nav className="flex flex-wrap gap-7 justify-center p-5 text-center text-xs sm:text-lg sm:gap-10 md:gap-15 text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
+            <nav className="flex flex-wrap gap-7 justify-center p-5 text-center text-xs sm:text-lg sm:gap-10 md:gap-15 text-white bg-purple-800/40 fixed min-w-[100vw] z-50 backdrop-blur-md">
                 <NavLink to= "/">Inicio</NavLink>
                 <NavLink to= "/videogames">Videojuegos</NavLink>
                 <NavLink to= "/about">Acerca de</NavLink>
@@ -22,7 +22,7 @@ function Layout(){
     }
     return (
         <div>
-            <nav className="flex flex-wrap gap-7 justify-center p-5 text-center text-xs sm:text-lg sm:gap-10 md:gap-15 text-white bg-purple-800/40 fixed min-w-[100vw] z-50">
+            <nav className="flex flex-wrap gap-7 justify-center p-5 text-center text-xs sm:text-lg sm:gap-10 md:gap-15 text-white bg-purple-800/40 fixed min-w-[100vw] z-50 backdrop-blur-md">
                 <NavLink to= "/">Inicio</NavLink>
                 <NavLink to= "/videogames">Videojuegos</NavLink>
                 <NavLink to= "/about">Acerca de</NavLink>
