f1ab2390fa0421713bb436af82aac274186194c3
zaxwtf <zaxwtf06@gmail.com>
2026-06-30 16:43:12 +0200

    style: titulo centrado

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,8 +9,8 @@ function Home(){
             <div className="bannerHome ">
                 <img className="w-[100vw] m-0 p-0" src="/fondo_GamerHub.jpg" alt="Fondo GamerHub" />
             </div>
-            <h1 className="text-4xl font-black">¡Bienvenido a GamerHub!</h1>
-            <h2 className="text-3xl my-8 font-black ml-5 mr-5 ">¡Todos tus Videojuegos favoritos al alcance de un click!</h2>
+            <h1 className="text-4xl font-black text-center">¡Bienvenido a GamerHub!</h1>
+            <h2 className="text-3xl my-8 font-black mx-5 ">¡Todos tus Videojuegos favoritos al alcance de un click!</h2>
             <LoadVideogames/>
             <section className="flex flex-wrap justify-center gap-[5vw] mx-0 mt-[10px]">
                 <Card title= "Biblioteca extensa" description= "Encuentra cualquier videojuego que busques, todo lo encontrarás en nuestra base de datos"/>
