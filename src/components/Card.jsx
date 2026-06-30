def1aa35b267d89c71db14f33c8ab8813691f363
zaxwtf <zaxwtf06@gmail.com>
2026-06-30 15:41:45 +0200

    styles: cambiado estilos de card, mas grosor al brode y el titulo se ve centrado y en negrita

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 function Card({title, description}){
     return(
-        <div className="rounded border border-black p-2 min-h-40 w-90">
-            <h2>{title}</h2>
+        <div className="rounded border border-black p-2 min-h-40 w-90 border-3">
+            <h2 className="font-black text-center">{title}</h2>
             <p>{description}</p>
         </div>
     )
