c22b3ca23db94a70c20cc077d91a73f4f91a6f5e
zaxwtf <zaxwtf06@gmail.com>
2026-07-01 19:34:11 +0200

    fix: agregado loading a la funcion para loguearse y mensaje de error solucionado, ahora sale correctamente

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
 function LoginForm({className}){
     const [data, setData] = useState({email:"", password:""})
     const {login} = useAuth()
-    const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState(null)
     const[isLoading, setIsLoading] = useState(false)
 
-    function handleChange(e){
-        const {name, value} = e.target
-        setData((prev)=>({...prev, [name]: value}))
-    }
+    // function handleChange(e){
+    //     const {name, value} = e.target
+    //     setData((prev)=>({...prev, [name]: value}))
+    // } Función puesta inline en el formulario siguiendo instrucciones del profesor
+
     const navigate = useNavigate()
 
     async function handleSubmit(e){
@@ -20,6 +21,7 @@ function LoginForm({className}){
         setErrors(null)
         setIsLoading(true)
         try {
+            setIsLoading(true)
             await login(data.email, data.password)
             navigate("/videogames")
         } catch (errors) {
@@ -27,20 +29,8 @@ function LoginForm({className}){
         }finally{
             setIsLoading(false)
         }
-        const foundedErrors = validate(data)
-        setErrors(foundedErrors)
 
     }
-
-    function validate(data){
-        const errors = {}
-
-        if (!data.email.includes("@")) errors.email = "email no valido"
-        if (data.password.length < 6) errors.password = "La contraseña debe tener mas de 6 caracteres"
-
-        return errors
-    }
-    
         
 
     return(
@@ -50,10 +40,9 @@ function LoginForm({className}){
             <div className="flex flex-col gap-1">
                 <label className="text-xl">Email</label>
                 <input onChange={(e)=> setData({...data, email: e.target.value})} type="email" name="email" value={data.email} className="rounded border border-gray-300 px-3 hover:border-purple-500 focus:outline-purple-500" />
-                
                 <label className="text-xl">Password</label>
                 <input onChange={(e)=> setData({...data, password: e.target.value})} type="password" name="password" value={data.password} className="rounded border border-gray-300 px-3 hover:border-purple-500 focus:outline-purple-500" />
-                
+                {errors && !isLoading && <span className="text-red-500">{errors}</span>}
                 <button className="rounded border border-purple-500 px-3 mt-6 hover:bg-purple-600 transition duration-400">Enviar</button>
                 
             </div>
