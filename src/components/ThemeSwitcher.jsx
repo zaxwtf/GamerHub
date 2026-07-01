7bdf2199e269e8748c64351df5ec72848f65e70b
zaxwtf <zaxwtf06@gmail.com>
2026-07-01 19:35:36 +0200

    feat: agregado sistema para que el boton cambie el valor de theme en el localstorage y el icono del boton tambien cambie segun que tema hay aplicado

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,14 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 function ThemeSwitcher(){
     const {theme, setTheme} = useContext(ThemeContext)
+    const [icon, setIcon] = useState("☀️")
 
     return(
-        <button onClick={()=>{
+        <button className="text-xs sm:text-3xl" onClick={()=>{
             setTheme(theme === "light" ? "dark" : "light")
+            setIcon(theme === "light" ? "🌙" : "☀️")
+            localStorage.setItem("dianoche", theme === "light" ? "dark" : "light")
         }}>
-            Cambiar de tema: {theme}
+            {icon}
         </button>
     )
 }
