4c38191a8263e332925d411e910a1fafa3685967
zaxwtf <zaxwtf06@gmail.com>
2026-07-01 22:28:17 +0200

    fix: arregaldo que al recargar pagina siempre sale el boton con el icono de sol indiferentemente de que tema tenga la pagina, ahora sale sol o luna segun que valor haya en localstorage

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -3,7 +3,7 @@ import { ThemeContext } from "../context/ThemeContext";
 
 function ThemeSwitcher(){
     const {theme, setTheme} = useContext(ThemeContext)
-    const [icon, setIcon] = useState("☀️")
+    const [icon, setIcon] = useState(localStorage.getItem("dianoche") === "dark" ? "🌙" : "☀️")
 
     return(
         <button className="text-xs sm:text-3xl" onClick={()=>{
